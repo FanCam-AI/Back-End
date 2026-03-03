c472a33997ba9d1dafee2ed0c65f32bf17400ef7
macbook_2 <qa7626@gmail.com>
2026-03-03 21:14:59 +0900

    Fix: allowed_hosts 테스트

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["fancamai.com", "*.fancamai.com", settings.CLOUD_RUN_URL]
+    allowed_hosts=["fancamai.com", "*.fancamai.com", settings.CLOUD_RUN_URL, "fancamai-backend-551954370080.europe-west1.run.app"]
 )
 
 app.include_router(auth_router)
