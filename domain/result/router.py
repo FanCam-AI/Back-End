aebf79402156942cfa713ca8efb3b7ba568ad953
macbook_2 <qa7626@gmail.com>
2026-03-05 02:10:43 +0900

    Fix: r2 업로드 오류 해결을 위한 코드 수정

--- domain/result/router.py
+++ domain/result/router.py
@@ -92,6 +92,7 @@ async def init_video_upload(
         ClientMethod="put_object",
         Params={
             "Bucket": settings.R2_BUCKET,
+            "ContentType": content_type,
             "Key": key,
         },
         ExpiresIn=600,  # 10분
