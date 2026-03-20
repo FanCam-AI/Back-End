ffbc5f7821f4dc3c907c123efcde8472996df379
macbook_2 <qa7626@gmail.com>
2026-03-21 02:08:27 +0900

    Fix: make result service 수정

--- domain/result/service.py
+++ domain/result/service.py
@@ -85,13 +85,21 @@ async def make_result_service(video_key, target_image_keys, spot_list, video_or_
         }
     }
 
+    if tracking_mode == "normal":
+        async with httpx.AsyncClient(timeout=10.0) as client:
+            response = await client.post(
+                f'https://{settings.CPU_RUNPOD_URL}.api.runpod.ai/process_run',
+                headers=headers,
+                json=data
+            )
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f'https://{settings.RUNPOD_URL}.api.runpod.ai/process_run',
-            headers=headers,
-            json=data
-        )
+    elif tracking_mode == "precision":
+        async with httpx.AsyncClient(timeout=10.0) as client:
+            response = await client.post(
+                f'https://{settings.GPU_RUNPOD_URL}.api.runpod.ai/process_run',
+                headers=headers,
+                json=data
+            )
 
     await redis_client.set(f"job_status:{user.id}", "processing", ex=25200)
 
