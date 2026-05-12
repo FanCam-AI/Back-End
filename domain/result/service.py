c4436b1cda984af93c5a3d9c06dc7da48e7d336d
macbook_2 <qa7626@gmail.com>
2026-05-13 02:37:39 +0900

    Fix: make_result processing 검사 추가

--- domain/result/service.py
+++ domain/result/service.py
@@ -37,10 +37,10 @@ def save_result_service(db: Session, title, file_path, file_type, user_id):
 
 
 async def make_result_service(video_key, target_image_keys, spot_list, video_or_gif, detection_model_type, tracking_mode, drag_box, user):
-    # status = await redis_client.get(f"job_status:{user.id}")
-    # if status == "processing":
-    #     delete_init_files_service(video_key, target_image_keys, settings.R2_BUCKET)
-    #     return {"status": "processing"}
+    status = await redis_client.get(f"job_status:{user.id}")
+    if status == "processing":
+        delete_init_files_service(video_key, target_image_keys, settings.R2_BUCKET)
+        return {"status": "processing"}
 
     f = Fernet(settings.FERNET_KEY)
     result_token = await create_result_token(user)
